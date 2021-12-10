49d8b8ee52f6e94db395c0feb890c1c6b9e73289
SanjulaD <desanjula99@gmail.com>
2021-12-10 17:03:12 +0530

    feat: splash screen added

--- product_management_system/main.cpp
+++ product_management_system/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include <QLatin1String>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
@@ -9,12 +11,18 @@ int main(int argc, char *argv[])
     QFile file(":/qss/qss/Devsion.qss");
     file.open(QFile::ReadOnly);
 
+    QSplashScreen *splash = new QSplashScreen;
+    splash -> setPixmap((QPixmap(":/splash/images/splash_image.png")));
+    splash->setFixedSize(1000, 400);
+    splash -> show();
+
     QString styleSheet { QLatin1String(file.readAll()) };
     Login w;
 
+    QTimer::singleShot(2400, splash, SLOT(close()));
+    QTimer::singleShot(2500, &w, SLOT(show()));
+
     //setup stylesheet
     w.setStyleSheet(styleSheet);
-
-    w.show();
     return a.exec();
 }
