9f56247b8e7f4e9387597680c583de3e7c6a37e1
SanjulaD <desanjula99@gmail.com>
2022-01-17 10:57:33 +0530

    feat: error imagse

--- build-product_management_system-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/debug/moc_productaddscreen.cpp
+++ build-product_management_system-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/debug/moc_productaddscreen.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../product_management_system/productaddscreen.h"
+#include "../../../../nilhan/product_management_system/productaddscreen.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
